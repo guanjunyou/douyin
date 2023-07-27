f28d644cfd1241e80124e84d1a9ef1f0459e2dbf
guanjunyou <2065642996@qq.com>
2023-07-27 10:58:23 +0800

    修复注册接口bug

--- service/UserService.go
+++ service/UserService.go
@@ -5,5 +5,7 @@ import "github.com/RaymondCode/simple-demo/models"
 type UserService interface {
 	GetUserById(Id int64) (models.User, error)
 
+	GetUserByName(name string) (models.User, error)
+
 	Save(user models.User) error
 }
