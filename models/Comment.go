9d2398446963e87a376f8262da793266ad49abc8
Dnull_P <dnull123p@gmail.com>
2023-07-29 01:26:15 +0800

    更新Comment Entity及对应的方法

--- models/Comment.go
+++ models/Comment.go
@@ -5,7 +5,64 @@ import "github.com/RaymondCode/simple-demo/utils"
 type Comment struct {
 	utils.CommonEntity
 	//Id         int64  `json:"id,omitempty"`
-	User       User   `json:"user"`
-	Content    string `json:"content,omitempty"`
-	CreateDate string `json:"create_date,omitempty"`
+	User    User   `json:"user"`
+	Content string `json:"content,omitempty"`
+}
+
+// CommentDB是数据库储存的Entity
+type CommentDB struct {
+	utils.CommonEntity
+	//Id         int64  `json:"id,omitempty"`
+	UserId  int64  `json:"user_id"`
+	VideoId int64  `json:"video_id"`
+	Content string `json:"content,omitempty"`
+}
+
+func (comment *Comment) ToCommentDB() CommentDB {
+	return CommentDB{
+		CommonEntity: comment.CommonEntity,
+		UserId:       comment.User.Id,
+		VideoId:      -1,
+		Content:      comment.Content,
+	}
+}
+
+func (commentDB *CommentDB) TableName() string {
+	return "comment"
+}
+
+func SaveComment(commentDB *CommentDB) error {
+	return utils.DB.Create(commentDB).Error
+}
+
+func GetCommentByVideoId(videoId int64) []Comment {
+	var comments []Comment
+	var commentDBs []CommentDB
+	// 找到对应User
+	GetUser := func(Id int64) (User, error) {
+		var user User
+
+		err := utils.DB.Where("id = ? AND is_deleted != ?", Id, 1).First(&user).Error
+		if err != nil {
+			return user, err
+		}
+		return user, nil
+	}
+
+	err := utils.DB.Where("video_id = ? AND is_deleted != ?", videoId, 1).Find(&commentDBs).Error
+	if err != nil {
+		return comments
+	}
+	for _, commentDB := range commentDBs {
+		user, err := GetUser(commentDB.UserId)
+		if err != nil {
+			user = User{Name: "未知用户"}
+		}
+		comments = append(comments, Comment{
+			CommonEntity: commentDB.CommonEntity,
+			User:         user,
+			Content:      commentDB.Content,
+		})
+	}
+	return comments
 }
