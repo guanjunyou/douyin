972caca06f31210b4005a56420eb92dba03e95fe
Dnull_P <dnull123p@gmail.com>
2023-07-29 01:29:08 +0800

    实现CommentService接口; 更新Controller逻辑;

--- controller/comment.go
+++ controller/comment.go
@@ -2,10 +2,17 @@ package controller
 
 import (
 	"github.com/RaymondCode/simple-demo/models"
+	"github.com/RaymondCode/simple-demo/service/impl"
 	"github.com/RaymondCode/simple-demo/utils"
 	"github.com/gin-gonic/gin"
+	"log"
 	"net/http"
+	"strconv"
 )
+
+func GetCommentService() impl.CommentServiceImpl {
+	return impl.CommentServiceImpl{}
+}
 
 type CommentListResponse struct {
 	models.Response
@@ -17,6 +24,16 @@ type CommentActionResponse struct {
 	Comment models.Comment `json:"comment,omitempty"`
 }
 
+func ParseVideoId(c *gin.Context) int64 {
+	video_id, err := strconv.ParseInt(c.Query("video_id"), 10, 64)
+	if err != nil {
+		log.Println(err)
+		c.JSON(http.StatusBadRequest, models.Response{StatusCode: 1, StatusMsg: "video_id is invalid"})
+		return -1
+	}
+	return video_id
+}
+
 // CommentAction no practical effect, just check if token is valid
 func CommentAction(c *gin.Context) {
 	token := c.Query("token")
@@ -25,14 +42,22 @@ func CommentAction(c *gin.Context) {
 	if user, exist := usersLoginInfo[token]; exist {
 		if actionType == "1" {
 			text := c.Query("comment_text")
-			c.JSON(http.StatusOK, CommentActionResponse{Response: models.Response{StatusCode: 0},
-				Comment: models.Comment{
-					CommonEntity: utils.NewCommonEntity(),
-					//Id:         1,
-					User:       user,
-					Content:    text,
-					CreateDate: "05-01",
-				}})
+			comment := models.Comment{
+				CommonEntity: utils.NewCommonEntity(),
+				//Id:         1,
+				User:    user,
+				Content: text,
+			}
+			video_id := ParseVideoId(c)
+			err := GetCommentService().PostComments(comment, video_id)
+			if err != nil {
+				log.Println(err)
+				c.JSON(http.StatusBadRequest, models.Response{StatusCode: 1, StatusMsg: "Comment failed"})
+				return
+			}
+
+			c.JSON(http.StatusOK, CommentActionResponse{Response: models.Response{StatusCode: 0, StatusMsg: "Comment success"},
+				Comment: comment})
 			return
 		}
 		c.JSON(http.StatusOK, models.Response{StatusCode: 0})
@@ -44,7 +69,7 @@ func CommentAction(c *gin.Context) {
 // CommentList all videos have same demo comment list
 func CommentList(c *gin.Context) {
 	c.JSON(http.StatusOK, CommentListResponse{
-		Response:    models.Response{StatusCode: 0},
-		CommentList: nil,
+		Response:    models.Response{StatusCode: 0, StatusMsg: "Comment list"},
+		CommentList: GetCommentService().CommentList(ParseVideoId(c)),
 	})
 }
