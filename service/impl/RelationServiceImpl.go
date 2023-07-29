271a77787940b26f761b17632e06b60cbe314dff
SurKaa <suralcarywn@qq.com>
2023-07-29 15:58:00 +0800

    Update RelationServiceImpl.go
    定义成扩展函数

--- service/impl/RelationServiceImpl.go
+++ service/impl/RelationServiceImpl.go
@@ -4,21 +4,21 @@ type RelationServiceImpl struct {
 }
 
 // FollowUser 关注用户
-func FollowUser(userId int64, toUserId int64, actionType int) error {
+func (relationServiceImpl RelationServiceImpl) FollowUser(userId int64, toUserId int64, actionType int) error {
 	return nil
 }
 
 // GetFollows 查询关注列表
-func GetFollows(userId int64) error {
+func (relationServiceImpl RelationServiceImpl) GetFollows(userId int64) error {
 	return nil
 }
 
 // GetFollowers 查询粉丝列表
-func GetFollowers(userId int64) error {
+func (relationServiceImpl RelationServiceImpl) GetFollowers(userId int64) error {
 	return nil
 }
 
 // GetFriends 查询好友列表
-func GetFriends(usrId int64) error {
+func (relationServiceImpl RelationServiceImpl) GetFriends(usrId int64) error {
 	return nil
 }
