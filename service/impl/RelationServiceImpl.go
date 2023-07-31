24f1ab830849039ccb856f86f663a854429fa69c
SurKaa <suralcarywn@qq.com>
2023-07-31 19:17:44 +0800

    Update RelationServiceImpl.go
    关注操作

--- service/impl/RelationServiceImpl.go
+++ service/impl/RelationServiceImpl.go
@@ -1,12 +1,37 @@
 package impl
 
-import "github.com/RaymondCode/simple-demo/models"
+import (
+	"fmt"
+	"github.com/RaymondCode/simple-demo/models"
+	"github.com/RaymondCode/simple-demo/utils"
+	"log"
+)
 
 type RelationServiceImpl struct {
 }
 
+// FollowResult 定义存储过程的返回值
+type followResult struct {
+	Cnt int
+}
+
 // FollowUser 关注用户
 func (relationServiceImpl RelationServiceImpl) FollowUser(userId int64, toUserId int64, actionType int) error {
+	var sql string
+	// 1 关注 2 取消
+	switch actionType {
+	case 1:
+		sql = "CALL addFollowRelation(?, ?)"
+	case 2:
+		sql = "CALL delFollowRelation(?, ?)"
+	default:
+		log.Println("非法actionType")
+	}
+	var result followResult
+	utils.GetMysqlDB().Raw(sql, userId, toUserId).Scan(&result)
+	if actionType != 2 && result.Cnt != 0 {
+		return fmt.Errorf("已经")
+	}
 	return nil
 }
 
