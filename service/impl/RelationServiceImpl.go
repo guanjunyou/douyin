88fc158a7658b9937d4f909b3b1a7654f121a1ba
SurKaa <suralcarywn@qq.com>
2023-07-31 21:34:48 +0800

    Update RelationServiceImpl.go
    完成社交的三个列表api

--- service/impl/RelationServiceImpl.go
+++ service/impl/RelationServiceImpl.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/RaymondCode/simple-demo/models"
 	"github.com/RaymondCode/simple-demo/utils"
-	"log"
 )
 
 type RelationServiceImpl struct {
@@ -25,7 +24,7 @@ func (relationServiceImpl RelationServiceImpl) FollowUser(userId int64, toUserId
 	case 2:
 		sql = "CALL delFollowRelation(?, ?)"
 	default:
-		log.Println("非法actionType")
+		return fmt.Errorf("非法actionType")
 	}
 	var result followResult
 	utils.GetMysqlDB().Raw(sql, userId, toUserId).Scan(&result)
@@ -37,15 +36,49 @@ func (relationServiceImpl RelationServiceImpl) FollowUser(userId int64, toUserId
 
 // GetFollows 查询关注列表
 func (relationServiceImpl RelationServiceImpl) GetFollows(userId int64) ([]models.User, error) {
-	return nil, nil
+	var users []models.User
+	err := utils.GetMysqlDB().Table("follow").Where("user_id = ? AND is_deleted != ?", userId, 1).Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
 // GetFollowers 查询粉丝列表
 func (relationServiceImpl RelationServiceImpl) GetFollowers(userId int64) ([]models.User, error) {
-	return nil, nil
+	var users []models.User
+	err := utils.GetMysqlDB().Table("follow").Where("follow_user_id = ? AND is_deleted != ?", userId, 1).Find(&users).Error
+	if err != nil {
+		return nil, err
+	}
+	return users, nil
 }
 
 // GetFriends 查询好友列表
-func (relationServiceImpl RelationServiceImpl) GetFriends(usrId int64) ([]models.User, error) {
-	return nil, nil
+func (relationServiceImpl RelationServiceImpl) GetFriends(userId int64) ([]models.User, error) {
+	follows, err := relationServiceImpl.GetFollows(userId)
+	if err != nil {
+		return nil, err
+	}
+	followers, err := relationServiceImpl.GetFollowers(userId)
+	if err != nil {
+		return nil, err
+	}
+	var friends []models.User
+	for _, user := range followers {
+		if containsID(follows, user.Id) {
+			friends = append(friends, user)
+		}
+	}
+	return friends, nil
+}
+
+// containsID 辅助函数，用于检查指定的 id 是否在数组中存在
+func containsID(arr []models.User, id int64) bool {
+	for _, u := range arr {
+		if u.Id == id {
+			return true
+		}
+	}
+	return false
 }
